cc0e1b8276644694edaec102343dfc0ef6e271ae
jpedrolourenco08 <jpedro.lourenco08@gmail.com>
2025-10-24 07:52:04 -0300

    Criando tabela - tb_site_contatos

--- database/migrations/2025_10_22_193809_create_site_contatos_table.php
+++ database/migrations/2025_10_22_193809_create_site_contatos_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', length: 50);
+            $table->string('telefone', length: 20);
+            $table->string('email', length: 80);
+            $table->integer('motivo_contato');
+            $table->text('mensagem');
         });
     }
 
