9425207312a302b4d4947dcb4c36dd94c9920267
jpedrolourenco08 <jpedro.lourenco08@gmail.com>
2026-03-09 06:38:37 -0300

    FEAT: implement middleware chaining for access logging and authentication

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,15 @@ Route::get('/login', function(){return "Login";})
   ->name('site.login');
 
 Route::prefix('/app')->  group(function(){
-  Route::get('/clientes',function(){return "Clientes";})->name('app.clientes');
-  Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
-  Route::get('/produtos', function(){return "Produtos";})->name('app.produtos');
+  Route::middleware('autenticacao')
+    ->get('/clientes',function(){return "Clientes";})
+    ->name('app.clientes');
+  Route::middleware('autenticacao')
+    ->get('/fornecedores', [FornecedorController::class, 'index'])
+    ->name('app.fornecedores');
+  Route::middleware('autenticacao')
+    ->get('/produtos', function(){return "Produtos";})
+    ->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', [TestController::class, 'test'])->name('teste');
